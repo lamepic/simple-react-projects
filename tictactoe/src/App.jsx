4de976ff01f6ddec484917a2ffcb9b90bd2ef4a5
Richard Arthur <richardarthur228@gmail.com>
2023-08-21 07:31:15 -0500

    initial setup

---
+++ tictactoe/src/App.jsx
@@ -0,0 +1,11 @@
+import GameBoard from "./components/GameBoard";
+
+function App() {
+  return (
+    <div className="h-screen grid place-items-center">
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ tictactoe/src/components/GameBoard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GameBoard() {
+  return <div className="w-5/12 h-4/6 border">GameBoard</div>;
+}
+
+export default GameBoard;
